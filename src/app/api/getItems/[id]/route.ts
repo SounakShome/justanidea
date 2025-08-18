e7e1366a0557b88b4c4d0f532e78888ce2144a79
SounakShome <sounakshome@gmail.com>
2025-08-18 23:07:01 +0530

    fixed getItems await for promise

--- src/app/api/getItems/[id]/route.ts
+++ src/app/api/getItems/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(req: Request, { params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     try {
         const data = await prisma.variants.findMany({
